6a6da17e66561375c139e65ba017a68c54c86e8d
cho45 <cho45@lowreal.net>
2011-10-25 12:48:17 +0900

    init

---
+++ lib/string.pm
@@ -0,0 +1,38 @@
+package string;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+string - 
+
+=head1 SYNOPSIS
+
+  use string;
+
+
+=head1 DESCRIPTION
+
+string is 
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
